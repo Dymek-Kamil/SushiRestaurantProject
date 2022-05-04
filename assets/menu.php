1cd2d3bbd424b03b13010bc48bbfdffbfecba02e
Dymek-Kamil <73522899+Dymek-Kamil@users.noreply.github.com>
2022-05-04 22:24:52 +0200

    Add files via upload

---
+++ assets/menu.php
@@ -0,0 +1,20 @@
+<div class="header">
+        <div class="menu-outer">
+            <div class="menu-logo-outer">
+                <img src="" alt="" class="menu-logo">
+            </div>
+            <div class="menu-menu">
+                <div class="menu-menu-outer">
+                    <a href="" class="menu-link">START</a>
+                    <a href="" class="menu-link">MENU</a>
+                    <a href="rezerwacja.html" class="menu-link">REZERWACJA</a>
+                    <a href="" class="menu-link">DOSTAWA</a>
+                    <a href="" class="menu-link">PROMOCJE</a>
+                    <a href="" class="menu-link">KONTAKT</a>
+                </div>
+            </div>
+            <div class="menu-cart-outer">
+                <img src="https://cdn-icons-png.flaticon.com/512/330/330474.png" alt="" class="menu-flag">
+                <a href="" class="menu-cart">Koszyk <i class="fa-solid fa-cart-shopping"></i></a>
+            </div>
+        </div>
